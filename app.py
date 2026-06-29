0ebc3b806c2d20ada6de9c230aad5c4183e6dda7
jbena22 <jhonattangbc@gmail.com>
2026-06-29 17:24:26 -0500

    feat: implementar flujo RAG con recuperación de contexto y respuestas usando Gemini

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from src.ingestion import procesar_documento
+from src.processor import crear_vector_store
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.title("🤖 Agente Inteligente Funcional")
 
@@ -16,4 +22,45 @@ if uploaded_file is not None:
         
         pregunta = st.text_input("¿Qué deseas saber sobre este documento?")
         if st.button("Consultar"):
-            st.info("Aquí conectaremos la lógica de la IA (RAG) próximamente.")
+            with st.spinner("Buscando en el documento..."):
+                
+                vectorstore = crear_vector_store(texto)
+
+                retriever = vectorstore.as_retriever(
+                    search_kwargs={"k": 3}
+                )
+
+                documentos = retriever.invoke(pregunta)
+
+                contexto = "\n\n".join(
+                    doc.page_content
+                    for doc in documentos
+                )
+                
+                llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+                prompt = ChatPromptTemplate.from_template("""
+                Eres un asistente especializado en responder preguntas únicamente utilizando la información del contexto.
+
+                Si la respuesta no está en el documento responde:
+
+                "No encontré esa información en el documento."
+
+                Contexto:
+                {contexto}
+
+                Pregunta:
+                {pregunta}
+                """)
+
+                cadena = prompt | llm
+
+                respuesta = cadena.invoke(
+                    {
+                        "contexto": contexto,
+                        "pregunta": pregunta
+                    }
+                )
+
+                st.write("### Respuesta:")
+                st.write(respuesta.content)
